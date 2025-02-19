d44b6b1772cc7464d4cd5710b071939a57d186e6
fufulame41 <luizz20garcia@gmail.com>
2025-02-19 10:43:14 -0500

    Update date.h

--- date.h
+++ date.h
@@ -1,1 +1,19 @@
 
+#ifndef DATE_H
+#define DATE_H
+#include <string>
+
+class Date {
+    protected:
+    std::string dateString;
+    int day;
+    int month;
+    int year;
+
+    public:
+    Date();
+    void init(const std::string& dateString);
+    void printDate();
+};
+
+#endif
