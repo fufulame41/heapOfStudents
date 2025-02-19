672982dde0ecb6b9f57113e73269f2408e2a7862
fufulame41 <luizz20garcia@gmail.com>
2025-02-19 10:42:04 -0500

    Update address.h

--- address.h
+++ address.h
@@ -1,1 +1,19 @@
 
+#ifndef ADDRESS_H
+#define ADDRESS_H
+#include <string>
+
+class Address{
+    protected:
+    std::string street;
+    std::string city;
+    std::string state;
+    std::string zip;
+
+    public:
+    Address()
+    void init(std::string street, std::string city, std::string state, std::string zip);
+    void printAddress();
+};
+
+#endif
