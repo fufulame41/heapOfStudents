2706a884c52cc545fb584dbcc1c92e36103d2e84
fufulame41 <luizz20garcia@gmail.com>
2025-02-21 16:55:43 -0500

    Update student.h

--- student.h
+++ student.h
@@ -1,1 +1,17 @@
+#include "data.h"
+#include "address.h"
+
+#include<iostream>
+#include<sstream>
+
+Student::student(){
+    studentString = "";
+    firstName = "";
+    lastName = "";
+    dob = new Date();
+    gradDate = new Date();
+    address = new Adress();
+    creditHours = 0;
+}
+
 
